eafb049540bb5483a4ccc20f869da18b84a26884
Rai Menezes <Raimenezes85@gmail.com>
2022-06-30 14:33:40 -0300

    first commit

--- App.js
+++ App.js
@@ -3,13 +3,26 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import Logo from './src/components/Logo';
 import Form from './src/components/Form';
 import Camera from './src/components/Camera';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name ="Logo" component={Logo} />
+      <Stack.Screen name ="Form" component={Form} />
+      <Stack.Screen name ="Camera" component={Camera} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      
-      <Form/>
-    </SafeAreaView>      
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>     
   );
 }
 
